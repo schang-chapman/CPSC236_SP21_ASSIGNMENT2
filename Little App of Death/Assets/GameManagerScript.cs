e9a5739472cd88303c21a8ef99fe309b2e80ad03
Sharon Chang <>
2021-02-26 21:36:29 -0800

    Added project

---
+++ Little App of Death/Assets/GameManagerScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using UnityEngine.UI;
+
+public class GameManagerScript : MonoBehaviour
+{
+    public string fileName = "default.txt";
+    public Text displayName;
+    private string[] studentNames;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        LoadStudentNames();
+    }
+
+    private void LoadStudentNames()
+    {
+        this.studentNames = File.ReadAllLines(fileName);
+    }
+
+    public void ButtonClick()
+    {
+        int i = UnityEngine.Random.Range(0, this.studentNames.Length);
+        string name = this.studentNames[i];
+        displayName.text = studentNames[i];
+    }
+}
